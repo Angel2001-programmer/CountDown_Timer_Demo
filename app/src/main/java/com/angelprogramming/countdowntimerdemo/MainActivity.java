496a17feed3e3e6cca4126d1c5bbd7b88097a104
Angel2001-programmer <angelwitchell2001@gmail.com>
2021-04-25 03:45:34 +0100

    port

--- app/src/main/java/com/angelprogramming/countdowntimerdemo/MainActivity.java
+++ app/src/main/java/com/angelprogramming/countdowntimerdemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.angelprogramming.countdowntimerdemo;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -20,8 +21,9 @@ public class MainActivity extends AppCompatActivity {
     Button btnStart;
     MediaPlayer alarm;
     SeekBar customSeekBar;
-    CountDownTimer countDownTimer = nul;
-    Boolean timer = false;
+    CountDownTimer countDownTimer = null;
+    Boolean mtimer = false;
+    long millisUntilFinished, mEndTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
         alarm = MediaPlayer.create(this, R.raw.alarm);
         customSeekBar = findViewById(R.id.seekBar);
 
+        if (savedInstanceState != null) {
+            millisUntilFinished = savedInstanceState.getLong("millisUntilFinished");
+        }
+
         customSeekBar.setProgress(0);
         customSeekBar.setMax(300);
         customSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -39,9 +45,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(final SeekBar seekBar, final int progress, boolean fromUser) {
                 update(progress);
-                if (!fromUser)
+                if (!fromUser) {
                     return;
-
+                }
             }
 
             @Override
@@ -62,8 +68,10 @@ public class MainActivity extends AppCompatActivity {
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (timer == false) {
-                    startClock();
+                if (mtimer != true) {
+                    mtimer = true;
+                    btnStart.setBackgroundResource(R.drawable.button_design_overlay);
+                    btnStart.setText("Stop!");
 
                     countDownTimer = new CountDownTimer(customSeekBar.getProgress() * 1000, 1000) {
                         @Override
@@ -73,29 +81,19 @@ public class MainActivity extends AppCompatActivity {
 
                         @Override
                         public void onFinish() {
-                            reset();
-                            if (alarm != null) {
-                                alarm.start();
-                            }
+                            restart();
                         }
-                    }.start();
+                    };
+                    countDownTimer.start();
                 } else {
-                    reset();
+                    restart();
                 }
             }
         });
     }
 
-    public void startClock() {
-        timer = true;
-        tvTimer.setText("Time left: " + "0:00");
-        btnStart.setText("Stop!");
-        btnStart.setBackgroundResource(R.drawable.button_design_overlay);
-
-    }
-
-    public void reset() {
-        timer = false;
+    public void restart() {
+        mtimer = false;
         tvTimer.setText("Time left: " + "0:00");
         countDownTimer.cancel();
         btnStart.setText("Go!");
@@ -120,16 +118,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putLong("customSeekBar", customSeekBar.getProgress() * 1000);
-        outState.putBoolean("timer", timer);
-        outState.putString("btnStart", btnStart.toString());
-        outState.putString("tvTimer", tvTimer.toString());
+        outState.putLong("millisUntilFinished", millisUntilFinished);
     }
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        timer = savedInstanceState.getBoolean("timer");
+        millisUntilFinished = savedInstanceState.getLong("millisUntilFinished");
 
     }
 }
